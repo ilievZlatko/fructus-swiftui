ee3bfbbb4f9692f35b345af15302d8c06e84fe60
zlatkoiliev <83271119+zlatkoiliev@users.noreply.github.com>
2021-07-02 14:40:52 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Zlatko Iliev on 2.07.21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
